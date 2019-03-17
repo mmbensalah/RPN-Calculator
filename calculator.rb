6d9ca9c63481f15884045fa9fad73fde787ef988
mmbensalah <mmbensalah@gmail.com>
2019-03-17 10:18:56 -0500

    Add code to evaluate method to zip integer and operator array

--- calculator.rb
+++ calculator.rb
@@ -10,6 +10,8 @@ class Calculator
     else
       integer_array << input.to_i
     end
+    integer_array.zip(operator_array).flatten.compact
+    #how to execute?
   end
 end
 
@@ -17,3 +19,5 @@ end
 #1. First step  - Need to take in number and operator through CL(comes in as string)
 #2. Second step - Need to differentiate b/w number and operator
 #3. Third step  - Need to store numbers and operators after differentiating
+#4. Fourth step - Need to zip arrays together to get integers and operators in
+#                 every other pattern - basic calculator when array executed
