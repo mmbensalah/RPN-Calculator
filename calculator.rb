6f5d3bbe18ed31d973e22aa91a8a12bace8661f5
mmbensalah <mmbensalah@gmail.com>
2019-03-17 15:20:29 -0500

    Refactor evaluate method by creating helper methods. Move 'gets' for calculator input to runner file out of evaluate method

--- calculator.rb
+++ calculator.rb
@@ -5,18 +5,30 @@ class Calculator
     @input_arr  = input_arr
   end
 
-  def evaluate
-    input = gets.chomp
-    if input == 'q'
-      exit
-    elsif ["+", "-", "/", "*"].any? { |operator| input.include? operator }
-      calculated_value = @input_arr[-2].send(input.to_sym, @input_arr[-1])
-      @input_arr.pop(2)
-      @input_arr << calculated_value
-      puts "= #{@input_arr.last}"
-    else
-      @input_arr  << input.to_i
+  def run(input)
+    if    quit?(input) then exit
+    elsif operator?(input) then evaluate(input)
+    else  stack(input)
     end
+  end
+
+  def quit?(input)
+    input == 'q'
+  end
+
+  def operator?(input)
+    ["+", "-", "/", "*"].any? { |operator| input.include? operator }
+  end
+
+  def evaluate(input)
+    calculated_value = @input_arr[-2].send(input.to_sym, @input_arr[-1])
+    @input_arr.pop(2)
+    @input_arr << calculated_value
+    puts "= #{@input_arr.last}"
+  end
+
+  def stack(input)
+    @input_arr << input.to_i
   end
 end
 
@@ -31,4 +43,5 @@ end
 #6. Sixth step  - Need to get rid of operator_array - will not work with RPN -
 #                 instead need to execute last two digits after the introduction
 #                 of every operator
-#7.Seventh step - Add q to exit 
+#7.Seventh step - Add q to exit
+#8. Eighth step - Refactor
