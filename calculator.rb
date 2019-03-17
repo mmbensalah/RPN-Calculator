e2b71b76219fbb1aa9983014b3f50bc60ac2d76b
mmbensalah <mmbensalah@gmail.com>
2019-03-17 09:38:34 -0500

    Create calculator class and portion of evaluate method

---
+++ calculator.rb
@@ -0,0 +1,13 @@
+class Calculator
+  def evaluate
+    integer_array = []
+    operator_array = []
+    input = gets.chomp
+
+  end
+end
+
+#Thought process after researching/understanding RPN and project requirements:
+#1. First step  - Need to take in number and operator through CL(comes in as string)
+#2. Second step - Need to differentiate b/w number and operator
+#3. Third step  - Need to store numbers and operators after differentiating
